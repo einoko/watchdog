499d39ef5d13bce549fe6eef15d5756c4d2c053e
einoko <sumire@mailbox.org>
2022-05-24 15:17:33 +0300

    Make TextService more robust

--- backend/services/textService.js
+++ backend/services/textService.js
@@ -1,23 +1,61 @@
 import puppeteer from "puppeteer";
 import { PuppeteerBlocker } from "@cliqz/adblocker-puppeteer";
 import fetch from "cross-fetch";
+import { promises as fs } from "fs";
+
+const scrollToBottom = async () => {
+  await new Promise((resolve) => {
+    const maxDistance = 10000; // in case there is infinite scroll, should be enough for most sites
+    const distance = 100;
+    const delay = 60;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      console.log(document.scrollingElement.scrollTop);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+          document.scrollingElement.scrollHeight ||
+        document.scrollingElement.scrollTop + window.innerHeight > maxDistance
+      ) {
+        clearInterval(timer);
+        resolve();
+      }
+    }, delay);
+  });
+};
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const getWebsiteText = async (url) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  PuppeteerBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+  const text = await PuppeteerBlocker.fromPrebuiltAdsAndTracking(fetch, {
+    path: "engine.bin",
+    read: fs.readFile,
+    write: fs.writeFile,
+  }).then(async (blocker) => {
     blocker.enableBlockingInPage(page);
+
+    try {
+      await page.goto(url);
+    } catch (err) {
+      await browser.close();
+      return null;
+    }
+
+    await page.evaluate(scrollToBottom);
+
+    await wait(1000);
+
+    const text = await page.evaluate(async () => {
+      const text = document.body.innerText;
+      return text;
+    });
+
+    return text ? text : null;
   });
 
-  try {
-    await page.goto(url);
-  } catch (err) {
-    await browser.close();
-    return null;
-  }
-
-  const text = await page.evaluate(() => document.body.innerText);
   await browser.close();
+
   return text;
 };
--- backend/tests/textService.test.js
+++ backend/tests/textService.test.js
@@ -4,7 +4,6 @@ import { getWebsiteText } from "../services/textService.js";
 
 describe("Get Website Text tests", () => {
   test("Get the text of a website (valid URL)", async () => {
-    jest.useFakeTimers();
     const text = await getWebsiteText("https://example.com");
 
     expect(text).toBeDefined();
@@ -13,7 +12,6 @@ describe("Get Website Text tests", () => {
   });
 
   test("Return null if the URL is invalid (or no text captured)", async () => {
-    jest.useFakeTimers();
     const text = await getWebsiteText(
       "https://4XY5UazbVPdYM85oCyKujXrchU9ER3AF.net"
     );
