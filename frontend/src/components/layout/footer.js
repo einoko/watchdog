db72300049dcbe00057a25840d6a6da65110c2ae
einoko <sumire@mailbox.org>
2022-06-01 15:49:55 +0300

    Add footer

--- frontend/src/components/layout/footer.js
+++ frontend/src/components/layout/footer.js
@@ -2,8 +2,14 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <header className="flex items-center justify-between flex-wrap bg-gray-200 p-6">
-      <div>This is a footer</div>
-    </header>
+    <footer className="bg-gray-800">
+      <div className="max-w-7xl mx-auto py-10 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="mt-8 md:mt-0 md:order-1">
+          <p className="text-center text-sm text-gray-400">
+            Watchdog – Self-hosted Website Monitoring
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
